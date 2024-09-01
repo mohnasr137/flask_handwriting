5a6e5c23a7fe98581a05a18049dfc80d714696ad
Mohamed Nasr <104239430+mohnasr137@users.noreply.github.com>
2024-09-01 04:33:58 +0300

    set

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def predict():
     return jsonify({"prediction": prediction})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port, debug=True)
